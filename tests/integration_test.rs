842f71030cf16e5b72e956bf831759879c6678cb
Chris Barnes <chrislloydbarnes@gmail.com>
2024-10-27 21:06:48 +0000

    even bigger test file

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,6 +2,11 @@ use djot_ast::AstNode;
 use rstest::{fixture, rstest};
 
 #[fixture]
+fn pandoc_json() -> &'static str {
+    include_str!("fixtures/pandoc-manual.json")
+}
+
+#[fixture]
 fn readme_dj() -> &'static str {
     include_str!("fixtures/readme.dj")
 }
@@ -11,7 +16,16 @@ fn readme_json() -> &'static str {
     include_str!("fixtures/readme.dj.json")
 }
 
+fn can_deser(s: &str) -> AstNode {
+    serde_json::from_str(s).unwrap()
+}
+
 #[rstest]
 fn deser_readme(readme_json: &str) {
-    let _node: AstNode = serde_json::from_str(readme_json).unwrap();
+    can_deser(readme_json);
+}
+
+#[rstest]
+fn deser_pandoc(pandoc_json: &str) {
+    can_deser(pandoc_json);
 }
